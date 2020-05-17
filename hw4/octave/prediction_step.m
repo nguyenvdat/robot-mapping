b2204d4dfae7b252c932f3c594812335c60a5bbe
Dat Nguyen <lonelyicevalley@icloud.com>
2020-05-17 11:38:30 +0700

    hw4 EKF Slam prediction step

--- hw4/octave/prediction_step.m
+++ hw4/octave/prediction_step.m
@@ -6,13 +6,22 @@ function [mu, sigma] = prediction_step(mu, sigma, u)
 % Use u.r1, u.t, and u.r2 to access the rotation and translation values
 
 % TODO: Compute the new mu based on the noise-free (odometry-based) motion model
+mu(1) = mu(1) + u.t*cos(mu(3)+u.r1);
+mu(2) = mu(2) + u.t*sin(mu(3)+u.r1);
+mu(3) = normalize_angle(mu(3) + u.r1 + u.r2);
 % Remember to normalize theta after the update (hint: use the function normalize_angle available in tools)
 
 
+N = (size(mu)(1) - 3)/2;
 % TODO: Compute the 3x3 Jacobian Gx of the motion model
+Gx = [1 0 -u.t*sin(mu(3)+u.r1);
+	  0 1  u.t*cos(mu(3)+u.r1);
+	  0 0  1];
 
 
 % TODO: Construct the full Jacobian G
+G = [Gx zeros(3,2*N);
+	 zeros(2*N,3) eye(2*N)];
 
 
 % Motion noise
@@ -24,6 +33,10 @@ R = zeros(size(sigma,1));
 R(1:3,1:3) = R3;
 
 % TODO: Compute the predicted sigma after incorporating the motion
-
-
+Sxx = sigma(1:3,1:3);
+Sxm = sigma(1:3, 4:end);
+Smx = sigma(4:end, 1:3);
+Smm = sigma(4:end, 4:end);
+sigma = [Gx*Sxx*Gx.' Gx*Sxm;
+		 (Gx*Sxm).' Smm] + R;
 end
