f70fee47d110f77326f40d71ae969a16306feac5
Dat Nguyen <lonelyicevalley@icloud.com>
2020-05-17 21:59:47 +0700

    hw4 EKF Slam correction step

--- hw4/octave/correction_step.m
+++ hw4/octave/correction_step.m
@@ -10,13 +10,14 @@ function [mu, sigma, observedLandmarks] = correction_step(mu, sigma, z, observed
 % The vector observedLandmarks indicates which landmarks have been observed
 % at some point by the robot.
 % observedLandmarks(j) is false if the landmark with id = j has never been observed before.
-
+N = (size(mu, 1)-3)/2;
 % Number of measurements in this time step
 m = size(z, 2);
 
 % Z: vectorized form of all measurements made in this time step: [range_1; bearing_1; range_2; bearing_2; ...; range_m; bearing_m]
 % ExpectedZ: vectorized form of all expected measurements in the same form.
 % They are initialized here and should be filled out in the for loop below
+m
 Z = zeros(m*2, 1);
 expectedZ = zeros(m*2, 1);
 
@@ -31,31 +32,48 @@ for i = 1:m
 	% If the landmark is obeserved for the first time:
 	if(observedLandmarks(landmarkId)==false)
 		% TODO: Initialize its pose in mu based on the measurement and the current robot pose:
+		mu(2*landmarkId+2) = mu(1) + z(i).range*cos(z(i).bearing + mu(3));
+		mu(2*landmarkId+3) = mu(2) + z(i).range*sin(z(i).bearing + mu(3));
 		
 		% Indicate in the observedLandmarks vector that this landmark has been observed
 		observedLandmarks(landmarkId) = true;
 	endif
 
 	% TODO: Add the landmark measurement to the Z vector
+	Z(2*i-1) = z(i).range;
+	Z(2*i) = z(i).bearing;
 	
 	% TODO: Use the current estimate of the landmark pose
 	% to compute the corresponding expected measurement in expectedZ:
+	sx = mu(2*landmarkId+2) - mu(1);
+	sy = mu(2*landmarkId+3) - mu(2);
+	s = [sx;sy];
+	q = s.'*s;
+	expectedZ(2*i-1) = sqrt(q);
+	expectedZ(2*i) = atan2(sy,sx) - mu(3);
+
 
 	% TODO: Compute the Jacobian Hi of the measurement function h for this observation
+	Hi = 1/q*[[-sqrt(q)*sx -sqrt(q)*sy 0; sy -sx -q] zeros(2, 2*landmarkId -2) [sqrt(q)*sx sqrt(q)*sy; -sy sx] zeros(2, 2*N - 2*landmarkId)];
 	
 	% Augment H with the new Hi
 	H = [H;Hi];	
 endfor
 
 % TODO: Construct the sensor noise matrix Q
+Q = 0.01*eye(2*m);
 
 % TODO: Compute the Kalman gain
+K = sigma*H.'/(H*sigma*H'+Q);
 
 % TODO: Compute the difference between the expected and recorded measurements.
 % Remember to normalize the bearings after subtracting!
 % (hint: use the normalize_all_bearings function available in tools)
+z_diff = normalize_all_bearings(Z - expectedZ);
 
 % TODO: Finish the correction step by computing the new mu and sigma.
 % Normalize theta in the robot pose.
+mu = mu + K*z_diff;
+sigma = (eye(2*N+3) - K*H)*sigma;
 
 end
