1eda8792a9a49769d706bc64e3c12a8563d666ba
Dat Nguyen <lonelyicevalley@icloud.com>
2020-05-19 17:38:25 +0700

    hw6 UKF Slam prediction step

--- hw6/octave/prediction_step.m
+++ hw6/octave/prediction_step.m
@@ -21,6 +21,9 @@ lambda = scale - n;
 % TODO: Transform all sigma points according to the odometry command
 % Remember to vectorize your operations and normalize angles
 % Tip: the function normalize_angle also works on a vector (row) of angles
+sigma_points(1,:) = sigma_points(1,:) + u.t*cos(sigma_points(3,:)+u.r1);
+sigma_points(2,:) = sigma_points(2,:) + u.t*sin(sigma_points(3,:)+u.r1);
+sigma_points(3,:) = normalize_angle(sigma_points(3,:) + u.r1 + u.r2);
 
 
 % Computing the weights for recovering the mean
@@ -30,9 +33,16 @@ wc = wm;
 % TODO: recover mu.
 % Be careful when computing the robot's orientation (sum up the sines and
 % cosines and recover the 'average' angle via atan2)
-
+mu_except_theta = sum(wm .* sigma_points(setdiff(1:n,3),:), 2);
+mu_cos = sum(wm .* cos(sigma_points(3,:)));
+mu_sin = sum(wm .* sin(sigma_points(3,:)));
+mu_theta = atan2(mu_sin, mu_cos);
+mu = [mu_except_theta(1:2); mu_theta; mu_except_theta(3:end)];
 
 % TODO: Recover sigma. Again, normalize the angular difference
+point_diff = sigma_points - mu;
+point_diff(3,:) = normalize_angle(point_diff(3,:));
+sigma = (wc .* point_diff) * point_diff';
 
 
 % Motion noise
@@ -44,6 +54,6 @@ R = zeros(size(sigma,1));
 R(1:3,1:3) = R3;
 
 % TODO: Add motion noise to sigma
-
+sigma = sigma + R;
 
 end
