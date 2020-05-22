047867ac7c12bb20025739b3470f186e612c2334
Dat Nguyen <lonelyicevalley@icloud.com>
2020-05-22 22:05:17 +0700

    hw10 Graph SLAM compute error of current graph

--- hw10/octave/compute_global_error.m
+++ hw10/octave/compute_global_error.m
@@ -6,7 +6,6 @@ Fx = 0;
 % Loop over all edges
 for eid = 1:length(g.edges)
   edge = g.edges(eid);
-
   % pose-pose constraint
   if (strcmp(edge.type, 'P') != 0)
 
@@ -16,6 +15,11 @@ for eid = 1:length(g.edges)
     %TODO compute the error of the constraint and add it to Fx.
     % Use edge.measurement and edge.information to access the
     % measurement and the information matrix respectively.
+    Z_ij = v2t(edge.measurement);
+    om = edge.information;
+    e_ij = t2v(inv(Z_ij)*(inv(x1)*x2));
+    Fx = Fx + e_ij'*om*e_ij;
+
 
   % pose-landmark constraint
   elseif (strcmp(edge.type, 'L') != 0)
@@ -25,6 +29,14 @@ for eid = 1:length(g.edges)
     %TODO compute the error of the constraint and add it to Fx.
     % Use edge.measurement and edge.information to access the
     % measurement and the information matrix respectively.
+    X_i = v2t(x);
+    z_il = edge.measurement;
+    om = edge.information;
+    R_i = X_i(1:2,1:2);
+    t_i = X_i(1:2,3);
+    e_il = R_i'*(l-t_i) - z_il;
+    Fx = Fx + e_il'*om*e_il;
+
 
   end
 
